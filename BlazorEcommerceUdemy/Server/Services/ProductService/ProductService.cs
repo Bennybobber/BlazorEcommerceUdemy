95f928ed54e328f6d9aa349d985f6407727414e8
Benjamin Sinyard <bennybobber22@gmail.com>
2022-07-10 10:44:37 +0100

    Best practices section completed

---
+++ BlazorEcommerceUdemy/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorEcommerceUdemy.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly DataContext _context;
+        public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<ServiceResponse<List<Product>>> GetProductListAsync()
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+
+            return response;
+        }
+    }
+}
