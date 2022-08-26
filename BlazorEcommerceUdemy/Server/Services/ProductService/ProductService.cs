431abb83297826f568a885d1019741b42c660151
Benjamin Sinyard <bennybobber22@gmail.com>
2022-08-26 10:06:47 +0100

    Added search functionality with predictive text

--- BlazorEcommerceUdemy/Server/Services/ProductService/ProductService.cs
+++ BlazorEcommerceUdemy/Server/Services/ProductService/ProductService.cs
@@ -37,6 +37,40 @@
             return response;
         }
 
+        public async Task<ServiceResponse<List<string>>> GetProductSearchSuggestions(string searchText)
+        {
+           var products = await FindProductsBySearchText(searchText);
+
+            List<string> result = new List<string>();
+            foreach (var product in products)
+            {
+                if(product.Title.Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Add(product.Title);
+                }
+
+                if(product.Description != null)
+                {
+                    // Splits up the description into individual words aside from punctuation
+                    var punctionation = product.Description.Where(char.IsPunctuation)
+                        .Distinct().ToArray();
+                    var words = product.Description.Split()
+                        .Select(s => s.Trim(punctionation));
+
+                    foreach (var word in words)
+                    {
+                        if(word.Contains(searchText, StringComparison.OrdinalIgnoreCase)
+                            && !result.Contains(word))
+                        {
+                            result.Add(word);
+                        }
+                    }
+                }
+            }
+                
+            return new ServiceResponse<List<string>> { Data = result };
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProudctsByCategory(string categoryUrl)
         {
             var response = new ServiceResponse<List<Product>>
@@ -48,6 +82,26 @@
             };
 
             return response;
+        }
+
+        public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                // Return products that have the search text in the title or the description
+                Data = await FindProductsBySearchText(searchText)
+            };
+            return response;
+        }
+
+        private async Task<List<Product>> FindProductsBySearchText(string searchText)
+        {
+            return await _context.Products
+                                .Where(p => p.Title.ToLower().Contains(searchText.ToLower())
+                                ||
+                                p.Description.ToLower().Contains(searchText.ToLower()))
+                                .Include(p => p.Variants)
+                                .ToListAsync();
         }
     }
 }
